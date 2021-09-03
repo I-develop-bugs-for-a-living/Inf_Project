20eea99f7709fa9b126c93c564cc770ca3213048
WhtsUpFam2 <87013572+WhtsUpFam2@users.noreply.github.com>
2021-09-03 09:41:00 +0200

    Menu start

--- menu.py
+++ menu.py
@@ -1,3 +1,14 @@
 import pygame
 import os
-import sys
+import pygame_menu
+
+pygame.init()
+
+Surface=pygame.display.set_mode((400,300))
+
+menu = pygame_menu.Menu("Cooles Game" , 400, 300, theme=pygame_menu.themes.THEME_DARK)
+menu.add.text_input("Name: ", default= "Hubi")
+menu.add.selector("Härtegrad:", [("Leicht",1), ("Hart", 2)])
+menu.add.button("Start", start_the_game)
+
+menu.mainloop(Surface)
